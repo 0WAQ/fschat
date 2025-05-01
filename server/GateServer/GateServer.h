edff4e360b385cc2d30e41b3bd48350c82ff71f8
0WAQ <2758282037@qq.com>
2025-05-02 01:57:10 +0800

    添加boost和jsoncpp

---
+++ server/GateServer/GateServer.h
@@ -0,0 +1,19 @@
+﻿#ifndef _GATESERVER_
+#define _GATESERVER_
+
+/**
+ * @brief 网关服务器
+ */
+class GateServer
+{
+public:
+
+	GateServer();
+	
+	~GateServer();
+
+private:
+
+};
+
+#endif // _GATESERVER_
